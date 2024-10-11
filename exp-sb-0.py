e2d3f61bf1a4e81eb43fa629f96ee2011f492d50
Uellington Damasceno <uellington99@gmail.com>
2024-10-11 15:58:38 -0300

    If applied, this commit will update the sb experiment.

--- exp-sb-0.py
+++ exp-sb-0.py
@@ -1,9 +1,9 @@
-from fogledger.iota.IotaBasic import (IotaBasic)
-from fogledger.iota.config.NodeConfig import (NodeConfig)
-from fogledger.iota.config.CoordConfig import (CoordConfig)
-from fogledger.iota.config.SpammerConfig import (SpammerConfig)
-from fogledger.iota.config.ApiConfig import (ApiConfig)
-from fogledger.iota.config.WebAppConfig import (WebAppConfig)
+from fogledgerIota.iota.IotaBasic import (IotaBasic)
+from fogledgerIota.iota.config.NodeConfig import (NodeConfig)
+from fogledgerIota.iota.config.CoordConfig import (CoordConfig)
+from fogledgerIota.iota.config.SpammerConfig import (SpammerConfig)
+from fogledgerIota.iota.config.ApiConfig import (ApiConfig)
+from fogledgerIota.iota.config.WebAppConfig import (WebAppConfig)
 from typing import List
 from fogbed import (
     VirtualInstance, setLogLevel, FogbedDistributedExperiment, Worker, Container, Controller
@@ -13,17 +13,23 @@ from datetime import datetime, timedelta
 import time
 
 
-
 setLogLevel('info')
 
-EXP_NAME = 'SB-0'
+EXP_NUM = '0'
+EXP_TYPE = '0'
+EXP_LEVEL = '0'
+API_URL = 'https://{s}:8080/api/latency-records/records'
+
+IS_BALANCEABLE = '0'
+IS_MULTI_LAYER = '0'
+
 COLLECT_TIME = '100'
 PUBLISH_TIME = '1000'
 SAMPLING_INTERVAL = '1000'
 LB_ENTRY_TIMEOUT = '10'
 TIMEOUT_LB_REPLY = '10'
 TIMEOUT_GATEWAY = '10'
-
+BUFFER_SIZE = '64'
 
 DEBUG_MODE_VALUE = 'true'
 INDEXES = 'LB_*'
@@ -35,10 +41,6 @@ DU_DLT_URL = ''
 DU_DLT_PORT = ''
 DU_ZMQ_SOCKET_URL = ''
 DU_ZMQ_SOCKET_PORT = ''
-
-IS_BALANCEABLE = ''
-IS_MULTI_LAYER = ''
-
 
 MIOTA1 = 'larsid01'
 MIOTA2 = 'larsid02'
@@ -65,15 +67,23 @@ QTD_MAX_DEVICES = 64
 QTD_DEVICES_PER_MACHINE = QTD_MAX_DEVICES // len(MGATEWAYS)
 LOAD_LIMIT = '16'
 MAX_QTD_DEVICE_PER_GATEWAYS = [32,32,0,0]
+MAX_QTD_GATEWAYS = 4
 
-
-
+def ip_generator():
+    ip_parts = list(map(int, "10.0.0.1".split('.')))
+    
+    for i in range(1000):
+        yield '.'.join(map(str, ip_parts))
+        for j in range(3, -1, -1):
+            if ip_parts[j] < 255:
+                ip_parts[j] += 1
+                break
+            ip_parts[j] = 0
 
 def createFotDevice(name: str, config: dict):
     return Container(
         name=name,
         dimage='udamasceno/virtual-fot-device:latest',
-        volumes=['experiments:/opt/exp'], # Atenção com essa porra aqui tenho que alterar o virtual device para salvar as paradas na pasta EXP
         environment={
             'DEVICE_ID': name,
             'BROKER_IP': config['broker_ip'],
@@ -81,9 +91,12 @@ def createFotDevice(name: str, config: dict):
             'USERNAME':config['username'],
             'PASSWORD': config['password'],
             'EXP_NUM': config['exp_num'],
+            'EXP_TYPE': config['exp_type'],
+            'EXP_LEVEL': config['exp_level'],
+            'API_URL': config['api_url'],
+            'BUFFER_SIZE': config['buffer_size']
         }
     )
-
 
 def createFotGatway(name: str, config: dict, mqtt_port: str = '1883'):
     return Container(
@@ -131,7 +144,7 @@ def create_connector():
 
 def create_zmq_bridge(iota_ip: str, indexes: str, port: str = '5556'):
     return Container(
-        name='zmq',
+        name='zmq/'+iota_ip,
         dimage='larsid/iota-zmq-bridge:1.0.0',
         dcmd=f'/entrypoint.sh',
         port_bindings={'5556': port},
@@ -143,91 +156,108 @@ def create_zmq_bridge(iota_ip: str, indexes: str, port: str = '5556'):
 
 setLogLevel('info')
 
-if (_name_ == '_main_'):
-
-    IS_BALANCEABLE = '0'
-    IS_MULTI_LAYER = '0'
+if (__name__ == '__main__'):
 
     exp = FogbedDistributedExperiment(
         controller_ip='localhost',
         controller_port=6633)
-    
+
     iota1_nodes = [NodeConfig(name='node0', port_bindings={'8081': '8081', '14265': '14265', '1883': '1883'})]
-    iota2_nodes = [NodeConfig(name='node1', port_bindings={'8081': '8081', '14265': '14265', '1883': '1883'})]
-
     iota1 = IotaBasic(exp=exp, prefix='iota1', conf_nodes=iota1_nodes)
-    iota2 = IotaBasic(exp=exp, prefix='iota2', conf_nodes=iota2_nodes)
-
+    iota1.name = 'i1'
+    iota1.ip = MIOTA1
     UP_DLT_URL = iota1.containers['node0'].ip
+    zmq1_bridge = create_zmq_bridge(UP_DLT_URL, INDEXES, UP_ZMQ_SOCKET_PORT)
+    iota1_virtual = exp.add_virtual_instance('iota1')
+
+    UP_ZMQ_SOCKET_URL = zmq1_bridge.ip
+    exp.add_docker(iota1, iota1_virtual)
+    exp.add_docker(zmq1_bridge, iota1_virtual)
+
+    iota1_worker = exp.add_worker(ip=MIOTA1, controller=Controller('localhost', 6633))
+    iota1_worker.add(iota1_virtual, reachable=True)
+
+    iota2_nodes = [NodeConfig(name='node1', port_bindings={'8081': '8081', '14265': '14265', '1883': '1883'})]
+    iota2 = IotaBasic(exp=exp, prefix='iota2', conf_nodes=iota2_nodes)
+    iota2.name = 'i2'
+    iota2.ip = MIOTA2
     DU_DLT_URL = iota2.containers['node1'].ip
+    zmq2_bridge = create_zmq_bridge(DU_DLT_URL, INDEXES, DU_ZMQ_SOCKET_PORT)
+    iota2_virtual = exp.add_virtual_instance('iota2')
+    DU_ZMQ_SOCKET_URL = zmq2_bridge.ip
+
+    exp.add_docker(iota2, iota2_virtual)
+    exp.add_docker(zmq2_bridge, iota2_virtual)   
+    iota2_worker = exp.add_worker(ip=MIOTA2, controller=Controller('localhost', 6633))
+    iota2_worker.add(iota2_virtual, reachable=True)
+
+    iota_workers = [iota1_worker, iota2_worker]
+
+    exp.add
+
+### Connector
+
+    connector = create_connector()
+    connector_virtual = exp.add_virtual_instance('connector')
+    exp.add_docker(connector, connector_virtual)
+
+    connector_worker = exp.add_worker(ip=MCONNECTOR, controller=Controller('localhost', 6633))
+
+    exp.add_tunnel(iota1_worker, connector_worker)
+    exp.add_tunnel(iota2_worker, connector_worker)
 
     UP_DLT_PORT = DU_DLT_PORT = '14265'
     UP_ZMQ_SOCKET_PORT = DU_ZMQ_SOCKET_PORT = '5556'
 
-    zmq1_bridge = create_zmq_bridge(UP_DLT_URL, INDEXES, UP_ZMQ_SOCKET_PORT)
-    zmq2_bridge = create_zmq_bridge(DU_DLT_URL, INDEXES, DU_ZMQ_SOCKET_PORT)
-
-    UP_ZMQ_SOCKET_URL = zmq1_bridge.ip
-    DU_ZMQ_SOCKET_URL = zmq2_bridge.ip
-
-    connector = create_connector()
-
-    iota1_virtual = exp.add_virtual_instance('iota1')
-    iota2_virtual = exp.add_virtual_instance('iota2')
-    connector_virtual = exp.add_virtual_instance('connector')
-    
     gateways = []
     gateway_name = ''
+
     mqtt_port = '188'
 
-    for i in range(MAX_QTD_DEVICE_PER_GATEWAYS):
+    for i in range(MAX_QTD_GATEWAYS):
         dlt_url = UP_DLT_URL if i % 2 == 0 else DU_DLT_URL
         dlt_port = UP_DLT_PORT if i % 2 == 0 else DU_DLT_PORT
+
         zmq_socket_url = UP_ZMQ_SOCKET_URL if i % 2 == 0 else DU_ZMQ_SOCKET_URL
         zmq_socket_port = UP_ZMQ_SOCKET_PORT if i % 2 == 0 else DU_ZMQ_SOCKET_PORT
         group = ('iota/i1' if i % 2 == 0 else 'iota/i2')
 
         gateway = createFotGatway(f'gateway{i}', {
-            'dlt_url': dlt_url,
-            'dlt_port': dlt_port,
-            'zmq_socket_url': zmq_socket_url,
-            'zmq_socket_port': zmq_socket_port,
-            'group': group,
-            'sampling_interval': SAMPLING_INTERVAL,
-            'load_limit': LOAD_LIMIT,
-            'lb_entry_timeout': LB_ENTRY_TIMEOUT,
-            'timeout_lb_reply': TIMEOUT_LB_REPLY,
-            'timeout_gateway': TIMEOUT_GATEWAY,
-            'collect_time': COLLECT_TIME,
-            'publish_time': PUBLISH_TIME,
-            'is_balanceable': IS_BALANCEABLE,
-            'is_multi_layer': IS_MULTI_LAYER,
+                        'dlt_url': dlt_url,
+                        'dlt_port': dlt_port,
+                        'zmq_socket_url': zmq_socket_url,
+                        'zmq_socket_port': zmq_socket_port,
+                        'group': group,
+                        'sampling_interval': SAMPLING_INTERVAL,
+                        'load_limit': LOAD_LIMIT,
+                        'lb_entry_timeout': LB_ENTRY_TIMEOUT,
+                        'timeout_lb_reply': TIMEOUT_LB_REPLY,
+                        'timeout_gateway': TIMEOUT_GATEWAY,
+                        'collect_time': COLLECT_TIME,
+                        'publish_time': PUBLISH_TIME,
+                        'is_balanceable': IS_BALANCEABLE,
+                        'is_multi_layer': IS_MULTI_LAYER,
         }, mqtt_port+str(i))
+        gateway.switch = f'gateway-{i}'
         gateways.append(gateway)
-        
-    exp.add_docker(iota1, iota1_virtual)
-    exp.add_docker(zmq1_bridge, iota1_virtual)
 
-    exp.add_docker(iota2, iota2_virtual)
-    exp.add_docker(zmq2_bridge, iota2_virtual)
 
-    exp.add_docker(connector, connector_virtual)
 
-    iota1_worker = exp.add_worker(ip=MIOTA1, controller=Controller('localhost', 6633))
-    iota2_worker = exp.add_worker(ip=MIOTA2, controller=Controller('localhost', 6633))
-    connector_worker = exp.add_worker(ip=MCONNECTOR, controller=Controller('localhost', 6633))
     devices = []
-
     for gateway, limit in zip(gateways, MAX_QTD_DEVICE_PER_GATEWAYS):
         print(f'Creating {limit} devices for gateway {gateway.ip}.')
         for i in range(limit):
             print(f'Creating device:{i}')
             device = createFotDevice(f'device-{i}', {
                 'broker_ip': gateway.ip,
-                'port': gateway.port_bindings['1883'],
+                'port': gateway.bindings['1883'],
                 'username': 'karaf',
                 'password': 'karaf',
-                'exp_num': EXP_NAME
+                'exp_num': EXP_NUM,
+                'exp_type': EXP_TYPE,
+                'exp_level': EXP_LEVEL,
+                'api_url': API_URL,
+                'buffer_size': BUFFER_SIZE
             })
             devices.append(device)
 
@@ -242,13 +272,13 @@ if (_name_ == '_main_'):
     gateway2_worker = exp.add_worker(ip=MGATEWAY2, controller=Controller('localhost', 6633))
     gateway3_worker = exp.add_worker(ip=MGATEWAY3, controller=Controller('localhost', 6633))
     gateway4_worker = exp.add_worker(ip=MGATEWAY4, controller=Controller('localhost', 6633))
-    
+
     gateways_workers = [gateway1_worker, gateway2_worker, gateway3_worker, gateway4_worker]
 
-    for indice, gateway in enumerate(gateways):
+    for indice, gateway in enumerate(gateways_virtual):
         worker = gateways_workers[indice % 4]
         worker.add(gateway, reachable=True)
-        print(f'Adding gateway {gateway.name} to worker {worker.ip}')
+        print(f'Adding gateway {gateway.label} to worker {worker.ip}')
 
     device1_worker = exp.add_worker(ip=MDEVICE1, controller=Controller('localhost', 6633))
     device2_worker = exp.add_worker(ip=MDEVICE2, controller=Controller('localhost', 6633))
@@ -256,12 +286,27 @@ if (_name_ == '_main_'):
     device4_worker = exp.add_worker(ip=MDEVICE4, controller=Controller('localhost', 6633))
 
     devices_workers = [device1_worker, device2_worker, device3_worker, device4_worker]
+    devices_virtual = []
 
-    for indice, device in enumerate(devices):
-        worker = devices_workers[indice % 4]
+    for i in range(len(MDEVICES)):
+        devices_virtual.append(exp.add_virtual_instance(f'devices-{i}'))
+
+    for device, virtual in zip(devices, devices_virtual):
+        exp.add_docker(device, virtual)
+        print(f'Adding device {device.ip} on {virtual.label}')
+
+    for indice, device in enumerate(devices_virtual):
+        worker = devices_workers[indice % len(devices_workers)]
         worker.add(device, reachable=True)
-        print(f'Adding device {device.environment['DEVICE_ID']} to worker {worker.ip}')
+        #print(f'Adding device {device.environment['DEVICE_ID']} to worker {worker.ip}')
 
+    for iota_worker in iota_workers:
+        for gateway_worker in gateways_workers:
+            exp.add_tunnel(iota_worker, gateway_worker)
+
+    for gw in gateways_workers:
+        for dw in devices_workers:
+            exp.add_tunnel(gw, dw)
 
     try:
         print("Experiment started")
@@ -274,7 +319,7 @@ if (_name_ == '_main_'):
         while datetime.now() - start_time < timedelta(minutes=10):
             time_remaining = timedelta(minutes=10) - (datetime.now() - start_time)
             print(f"Time reaming: {time_remaining}")
-        
+
         print("Stopping the experiment...")
         exp.stop()
     except Exception as ex:
